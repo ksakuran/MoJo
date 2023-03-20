43eb7f826db0350564ace30fb2795dbbf2ae76cf
Katherine Nishimura <katherine.nishimura@live.ca>
2023-03-20 09:52:59 -0700

    update schema and seedfiles, write schema for users and journal

---
+++ back-end/db/schema/5_journals.sql
@@ -0,0 +1,7 @@
+-- Drop and recreate Users table
+DROP TABLE IF EXISTS journals CASCADE;
+CREATE TABLE journals (
+  id SERIAL PRIMARY KEY NOT NULL,
+  body TEXT, 
+  day_selection_id INTEGER REFERENCES day_selections(id) ON DELETE CASCADE
+)

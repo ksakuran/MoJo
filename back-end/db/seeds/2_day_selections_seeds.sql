b9da648d8dac5362226dc872ee5d5f575567a06f
cchand-vanc <cmchandler@live.co.uk>
2023-03-20 12:25:36 -0700

    Add seeds for day_selections table

--- back-end/db/seeds/2_day_selections_seeds.sql
+++ back-end/db/seeds/2_day_selections_seeds.sql
@@ -1,6 +1,11 @@
--- INSERT INTO favourites
--- (user_id,map_id)
--- VALUES
--- (8,3),(8,2),
--- (10,1),(10,2),(10,3),(10,4),
--- (16,3),(8,4),(11,1),(12,4),(12,2);
+INSERT INTO day_selections
+(weather_description, weather_icon, created_date, user_id)
+VALUES
+('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', 2023-03-15, 1),
+('Rainy', 'https://cdn.weatherapi.com/weather/64x64/day/266.png', 2023-03-16, 1),
+('Sunny', 'https://cdn.weatherapi.com/weather/64x64/day/113.png', 2023-03-17, 1),
+('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', 2023-02-22, 1),
+('Sunny', 'https://cdn.weatherapi.com/weather/64x64/day/113.png', 2023-02-23, 1),
+('Rainy', 'https://cdn.weatherapi.com/weather/64x64/day/266.png', 2023-02-24, 1);
+
+

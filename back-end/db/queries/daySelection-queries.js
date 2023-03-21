a82f1a811e02030aa110cc8fb1c6da99edb1c204
kmomozin <khinmomozin@gmail.com>
2023-03-20 20:03:43 -0600

    add route and queries for day selection

--- back-end/db/queries/daySelection-queries.js
+++ back-end/db/queries/daySelection-queries.js
@@ -1,19 +1,77 @@
 const db = require('../connection');
 
-const getContributorByUserAndMap = (userId, mapId) => {
-  // return db.query(`SELECT *
-  // FROM contributors
-  // WHERE user_id = $1
-  // AND map_id = $2;
-  // `, [userId, mapId])
-  //   .then(data => {
-  //     return data.rows;
-  //   })
-  //   .catch(err => {
-  //     return console.error(err);
-  //   });
+const getDaySelectionByDate = (date) => {
+  return db.query(`
+    SELECT * 
+    FROM day_selections 
+    WHERE created_date = $1;
+  `, [date])
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(err => {
+      return console.error(err);
+    });
+};
+
+const getMoodSelectionByDay = (startDate, endDate) => {
+  return db.query(`
+    SELECT day_selections.id as day_selection_id, mood_selections.id as mood_selection_id, 
+            mood_selections.created_date as mood_selection_created_date, mood_id, rating,
+            moods.name, moods.color
+    FROM day_selections  
+      JOIN mood_selections ON day_selections.id = mood_selections.day_selection_id
+      JOIN moods ON moods.id = mood_selections.mood_id
+    WHERE day_selections.created_date BETWEEN $1 AND $2;
+  `, [startDate, endDate])
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      return console.error(err);
+    });
+};
+
+const getJournalDaySelectionByDay = (startDate, endDate) => {
+  return db.query(`
+    SELECT day_selections.id as day_selection_id, weather_description, weather_icon, 
+          day_selections.created_date as day_selection_created_date, user_id, 
+	        journals.id as journal_id, body
+    FROM day_selections  
+      LEFT JOIN journals ON day_selections.id = journals.day_selection_id
+    WHERE day_selections.created_date BETWEEN $1 AND $2;
+  `, [startDate, endDate])
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      return console.error(err);
+    });
+};
+
+const addDaySelection = (body) => {
+  const query = `
+  INSERT INTO day_selections
+  ( weather_description, weather_icon, created_date, user_id)
+  VALUES
+  ($1,$2,$3, $4)
+  RETURNING *;
+  `;
+
+  const param = [body.weather_description, body.weather_icon, body.created_date, body.user_id];
+
+  return db.query(query, param)
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(err => {
+      return err;
+    });
 };
 
 module.exports = {
-  getContributorByUserAndMap
+  getDaySelectionByDate,
+  getMoodSelectionByDay,
+  getJournalDaySelectionByDay,
+  addDaySelection
 };
--- back-end/routes/daySelection-router.js
+++ back-end/routes/daySelection-router.js
@@ -1,18 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const daySelectionQueries = require('../db/queries/daySelection-queries');
+const date = require('../helper/date.js');
 
-router.get(`/`, (req, res) => {
-  // const mapId = req.params.mapid;
-  // pointQueries.getPointsDetailsByMapId(mapId)
-  //   .then(results => {
-  //     console.log("results:", results);
-  //     return res.json({ results });
-  //   })
-  //   .catch(err => {
-  //     res
-  //       .status(500)
-  //       .json({ error: err.message });
-  //   });
+
+//returns an object of day selection details for selected date
+router.get(`/:date`, (req, res) => {
+  const date = req.params.date;
+  daySelectionQueries.getDaySelectionByDate(date)
+    .then(daySelection => {
+      return res.json({ daySelection });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+//returns
+//an object of day selection details and journal data for selected date
+//an array of mood data for selected date
+router.get(`/calendar/:month/:year`, (req, res) => {
+  const month = req.params.month;
+  const year = req.params.year;
+  const { startDate, endDate } = date.getStartAndEndDayInMonth(year, month);
+
+  const moodPromise = daySelectionQueries.getMoodSelectionByDay(startDate, endDate);
+  const journalPromise = daySelectionQueries.getJournalDaySelectionByDay(startDate, endDate);
+
+  Promise.all([moodPromise, journalPromise])
+    .then(calendarData => {
+      res.json({ calendarData });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+//create new day selection object and return current selected day_selections_id
+router.post('/new', (req, res) => {
+  const body = req.body;
+  daySelectionQueries.addDaySelection(body)
+    .then(daySelection => {
+      res.json({ daySelection });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
 });
 
 module.exports = router;

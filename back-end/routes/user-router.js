285442cdf26eb6604c86d8a3ca96584c1983a96b
cchand-vanc <cmchandler@live.co.uk>
2023-03-20 22:44:18 -0700

    Add /:userId route

--- back-end/routes/user-router.js
+++ back-end/routes/user-router.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const userQueries = require('../db/queries/user-queries');
 
-router.get(`/`, (req, res) => {
-  // const mapId = req.params.mapid;
-  // pointQueries.getPointsDetailsByMapId(mapId)
-  //   .then(results => {
-  //     console.log("results:", results);
-  //     return res.json({ results });
-  //   })
-  //   .catch(err => {
-  //     res
-  //       .status(500)
-  //       .json({ error: err.message });
-  //   });
+// Returns user's information
+router.get(`/:userId`, (req, res) => {
+  const userId = req.params.userId;
+  userQueries.getUserInfo(userId)
+    .then(results => {
+      console.log('body: ', results);
+      return res.json(results);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
 });
+
+
+// Updates user's information
+router.post(`/:userId`, (req, res) => {
+  const body = req.body
+
+  userQueries.updateUserInfo(body)
+    .then(results => {
+      console.log('results', results);
+      return res.json(results);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
 
 module.exports = router;

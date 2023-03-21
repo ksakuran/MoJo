4ac2743076ce51958cc6b839728494532321ee99
cchand-vanc <cmchandler@live.co.uk>
2023-03-20 22:45:00 -0700

    Refactored conditional statements

--- back-end/routes/journal-router.js
+++ back-end/routes/journal-router.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getJournalBody, saveJournalBody, updateJournalBody } = require('../db/queries/journal-queries')
+const journalQueries = require('../db/queries/journal-queries');
 
-// returns body of journal entry from journals table
+// Returns body of journal entry from journals table
 router.get(`/:daySelectionId`, (req, res) => {
 
   const daySelectionId = req.params.daySelectionId;
-  getJournalBody(daySelectionId)
+  journalQueries.getJournalBody(daySelectionId)
     .then(results => {
-      console.log('body: ', results);
       return res.json(results);
     })
     .catch(err => {
@@ -19,35 +18,28 @@ router.get(`/:daySelectionId`, (req, res) => {
 });
 
 
-//checks if journal entry exists already, then updates or creates accordingly
+// Checks if journal entry exists already, then updates or creates accordingly
 router.post(`/`, (req, res) => {
 
   const journalBody = req.body;
 
-  getJournalBody(journalBody.daySelectionId)
-  .then(results => {
-    //if entry already exists
-    console.log('results', results)
-    let journalPromise
-    if (!results) {
-      journalPromise = saveJournalBody(journalBody)
-    }
-    //if entry does not already exist
-    else {
-     journalPromise = updateJournalBody(journalBody)
-    }
-    journalPromise
-      .then(results => {
-        console.log('results', results);
-        return res.json(results);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-  
-})
+  journalQueries.getJournalBody(journalBody.daySelectionId)
+    .then(results => {
+      let journalPromise;
+      
+      results ? journalPromise = journalQueries.updateJournalBody(journalBody) : journalPromise = journalQueries.saveJournalBody(journalBody)
+      
+      journalPromise
+        .then(results => {
+          return res.json(results);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    });
+
+});
 
 module.exports = router;

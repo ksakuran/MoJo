b1cec88da362538dc0141eba4f435b8e425f6159
cchand-vanc <cmchandler@live.co.uk>
2023-03-21 17:37:36 -0700

    Add import for style sheet

---
+++ front-end/src/components/Journal/JournalPage.js
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import Journal from "./Journal";
+import JournalChecklist from "./JournalChecklist";
+import "./../../styles/JournalPage.scss";
+
+function JournalPage(){
+
+  return (
+    <section className="page" id="journal-page">
+      <Journal />
+      <JournalChecklist />
+    </section>
+  )
+};
+
+export default JournalPage;

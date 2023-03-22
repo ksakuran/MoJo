5437c41fbdbea5cd7f6497cccc7ac28723afb316
cchand-vanc <cmchandler@live.co.uk>
2023-03-21 21:21:07 -0700

    Add component for chnage date button

---
+++ front-end/src/components/SideNav/ChangeDate.js
@@ -0,0 +1,13 @@
+import classNames from "classnames";
+import "./../../styles/ChangeDate.scss"; 
+
+function ChangeDate() {
+
+  return (
+    <div>
+      <button className="date-btn">view different day</button>
+    </div>
+  )
+};
+
+export default ChangeDate

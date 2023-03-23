05ede2d5cb4d0e04b384870761a1a089054f5135
kmomozin <khinmomozin@gmail.com>
2023-03-23 11:53:39 -0600

    fix change date viewmode issue

--- front-end/src/components/SideNav/ChangeDate.js
+++ front-end/src/components/SideNav/ChangeDate.js
@@ -4,18 +4,25 @@ import "../../styles/ChangeDate.scss";
 import Button from "../Common/Button";
 import { appContext } from '../../providers/AppProvider';
 import Icon from './../Common/Icon';
+import { daySelectionContext } from './../../providers/DaySelectionProvider';
 
 function ChangeDate() {
 
   const changeDateClass = classNames("change-date");
 
   const { setViewMode } = useContext(appContext);
+  const { setSelectedDate } = useContext(daySelectionContext);
+
+  const handleChangeDate = () => {
+    setSelectedDate('');
+    setViewMode('CALENDAR');
+  };
 
   return (
     <div className={changeDateClass}>
       <Icon imgUrl='images/icons/change_date.png' />
       <Button
-        onClickHandler={() => setViewMode('CALENDAR')}
+        onClickHandler={handleChangeDate}
       >Change Date</Button>
     </div>
   );

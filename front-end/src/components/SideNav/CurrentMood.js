090e36e19eef549979ed3527f167a54203e68f95
cchand-vanc <cmchandler@live.co.uk>
2023-03-22 18:44:46 -0700

    Add axios and useContext

--- front-end/src/components/SideNav/CurrentMood.js
+++ front-end/src/components/SideNav/CurrentMood.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import axios from 'axios';
 import classNames from "classnames";
 import "./../../styles/CurrentMood.scss";
 
+import Icon from '../Common/Icon';
+import { appContext } from '../../providers/AppProvider';
+
+
 function CurrentMood() {
 
-  const moods = ['happy', 'motivated', 'grateful'];
-  // mood_selections
+  const { daySelectionId } = useContext(appContext);
+  let moods = [];
 
-  const iconSelection = function() {
-    for (let mood of moods) {
-      // mood === moods.name ? return moods.icon
-    }
-  };
+  // useEffect(() => {
+  //   axios
+  //     .get(`/api/mood/${daySelectionId}`)
+  //       .then(response => {
+  //         console.log("user's moods today: ", response);
+  //         moods.push(response);
+  //       })
+  //       .catch(err => {
+  //         console.log("error:", err.message);
+  //       });
+  // }, [daySelectionId, moods]);
 
-  const currentMood = moods.map(mood => {
+
+  const currentMood = moods[0].map(mood => {
     return (
-      <li><i>{iconSelection()}</i>{mood}</li>
+    <li>
+      <Icon imgUrl={`images/${mood.name}.png`} iconSize="small">{mood.name}</Icon>/
+    </li>
     );
   });
+
 
   return (
     <div>

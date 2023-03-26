658433453544154b5ea1eaeee297fe634d35da81
cchand-vanc <cmchandler@live.co.uk>
2023-03-25 23:40:47 -0700

    Add components and styling for user checklist

--- front-end/src/components/Journal/JournalChecklist.js
+++ front-end/src/components/Journal/JournalChecklist.js
@@ -1,26 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import classNames from "classnames";
 import "./../../styles/JournalChecklist.scss";
+import axios from 'axios';
+
 import JournalChecklistItem from "./JournalChecklistItem";
+import { appContext } from './../../providers/AppProvider';
+import { daySelectionContext } from '../../providers/DaySelectionProvider';
+
 
 function JournalCheckist(props) {
-  // const checklistItems = props.items.map((item) => {
-  //   return <JournalChecklistItem
-  //       id={props.item_id}
-  //       description={props.description}
-  //       user={props.user_id}
-  //       isSelected={props.selected}
-  //       isActive={props.active}
-  //       />
+  const checklistClass = classNames("checklist-container");
+
+  const { userId } = useContext(appContext);
+  const { daySelectionId } = useContext(daySelectionContext);
+
+  const [checklist, setChecklist] = useState("");
+  const [selectedItems, setSelectedItems] = useState("");
+  const [newItem, setNewItem] = useState(false);
+
+  const updateListItems = () => {
+    setNewItem(true);
+  };
+
+  //get active list data, 
+  useEffect(() => {
+    axios
+      .get(`/api/item/${userId}`)
+      .then(res => {
+        console.log('res', res);
+        setChecklist(res.data[0]);
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  }, [newItem]);
+
+
+  // get selected list items 
+  useEffect(() => {
+    axios
+      .get(`/api/selection/${daySelectionId}`)
+      .then(results => {
+        console.log('results', results);
+        setSelectedItems(results.data[0]); //[0] because [1] is the inactive ones
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  }, [newItem]);
+
+
+  // check off an item
+  useEffect(() => {
+    axios
+      .put(`/api/selection/new`, {
+        daySelectionId: daySelectionId,
+        itemId: checklist.id
+      })
+      .then(response => {
+        console.log("checklist item selected!", response);
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  }, [newItem]);
+
+
+  // picks through checklist, returns true if item is selected, and false if not
+  const determineIsSelected = () => {
+    return checklist.some(item => selectedItems.includes(item));
+  };
+
+
+  // const checklistItems = checklist.map((item) => {
+  //     return <JournalChecklistItem
+  //
+  // What are the value in the checklist object that's returned from the DB?
+  //       id={checklist.item_id}
+  //       description={checklist.description}
+  //       user={checklist.user_id}
+  //       isSelected={() => determineIsSelected()}
+  //       isActive={checklist.active}
+  //       newItem={updateListItems}
+  //     />
+  //   }
   // })
 
+
   return (
-    <article>
+
+    <article className={checklistClass}>
+      <h3>take note of what you did today</h3>
       <ul>
         <JournalChecklistItem />
         <JournalChecklistItem />
         <JournalChecklistItem />
-
         {/* {checklistItems} */}
       </ul>
     </article>
--- front-end/src/components/Journal/JournalChecklistItem.js
+++ front-end/src/components/Journal/JournalChecklistItem.js
@@ -4,10 +4,14 @@ import "./../../styles/JournalChecklistItem.scss";
 
 function JournalChecklistItem(props) {
 
+  const checkboxClass = classNames("checkbox");
+  const checklistItemClass = classNames("checklist-item");
+
+
   return (
-    <li>
-      <input type="checkbox" name="checklistItemBox" id="checklistItemBox" />
-      <label htmlFor="checklistItemBox"> Checklist Item</label>
+    <li className={checklistItemClass}>
+      <input className={checkboxClass} type="checkbox" value="" id="checklistItemBox"/>
+      <label htmlFor="checklistItemBox">checklist item</label>
       {/* {props.name} to go in label */}
     </li>
   );

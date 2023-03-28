d2e8c05412e6e2a9c3e284f3fdb19779c690fede
Katherine Nishimura <katherine.nishimura@live.ca>
2023-03-28 13:02:29 -0700

    mojo jojo default

--- back-end/db/seeds/1_users_seeds.sql
+++ back-end/db/seeds/1_users_seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO users
 (first_name, last_name, city, password, email, profile_image)
 VALUES
-('Jane', 'Doe', 'Calgary', '123', 'jane@gmail.com','https://cdn.pixabay.com/photo/2021/07/25/08/03/account-6491185_1280.png');
+('Mojo', 'Jojo', 'Vancouver', 'powerpuff', 'mojo_jojo@gmail.com','https://www.dictionary.com/e/wp-content/uploads/2018/04/Mojo-Jojo.jpg');

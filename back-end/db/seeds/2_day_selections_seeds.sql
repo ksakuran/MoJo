5dbca1da6d53063ea775116ff6d4b2304838ef11
kmomozin <khinmomozin@gmail.com>
2023-03-29 18:07:49 -0600

    add seed

--- back-end/db/seeds/2_day_selections_seeds.sql
+++ back-end/db/seeds/2_day_selections_seeds.sql
@@ -1,10 +1,10 @@
 INSERT INTO day_selections
 (weather_description, weather_icon, created_date, user_id)
 VALUES
-('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', '2023-02-08', 1),
-('Rainy', 'https://cdn.weatherapi.com/weather/64x64/day/266.png', '2023-02-17', 1),
-('Sunny', 'https://cdn.weatherapi.com/weather/64x64/day/113.png', '2023-02-18', 1),
-('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', '2023-02-22', 1),
+('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', '2023-02-24', 1),
+('Rainy', 'https://cdn.weatherapi.com/weather/64x64/day/266.png', '2023-02-26', 1),
+('Sunny', 'https://cdn.weatherapi.com/weather/64x64/day/113.png', '2023-02-27', 1),
+('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', '2023-02-28', 1),
 ('Sunny', 'https://cdn.weatherapi.com/weather/64x64/day/113.png', '2023-03-06', 1),
 ('Rainy', 'https://cdn.weatherapi.com/weather/64x64/day/266.png', '2023-03-10', 1),
 ('Cloudy', 'https://cdn.weatherapi.com/weather/64x64/day/122.png', '2023-03-15', 1),
